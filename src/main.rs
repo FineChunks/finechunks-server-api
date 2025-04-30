3e0f17aac0bd77d3e81ad730ac0601920333f44d
Iémélian RAMBEAU <iemelian.rambeau@student.junia.com>
2025-04-30 23:28:38 +0200

    fix: api trop nul va plus loin

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,7 @@ async fn main() {
         }))
         .layer(cors);
 
-    // run our app with hyper, listening globally on port 3000
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    // run our app with hyper, listening globally on port 3001
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3001").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
